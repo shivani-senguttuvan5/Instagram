c80ccc329a381d61dcfad0e158155d5dc30f031a
shivani-senguttuvan5 <shivani.senguttuvan5@gmail.com>
2022-10-14 09:08:41 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Shivani on 10/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
